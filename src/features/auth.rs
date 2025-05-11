1d952202d437cc9c0829c3449a7c145f171c575e
jarne-vercruysse <JarneVercruysse@proton.me>
2025-05-12 01:43:37 +0200

    feat: organization per feature

---
+++ src/features/auth.rs
@@ -0,0 +1,18 @@
+#[server]
+fn login_user() {}
+
+#[server]
+fn logout_user() {}
+
+#[server]
+fn register_user() {}
+
+#[component]
+fn LoginPage() -> impl IntoView {
+    todo!()
+}
+
+#[component]
+fn RegistrationPage() -> impl IntoView {
+    todo!()
+}
